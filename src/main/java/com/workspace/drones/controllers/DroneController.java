b2e161eb1c9012d59444dfac13de71ba55d2d346
Saulo0097 <saulohermida1997@gmail.com>
2024-02-02 12:08:04 -0500

    adding CROSS permission

--- src/main/java/com/workspace/drones/controllers/DroneController.java
+++ src/main/java/com/workspace/drones/controllers/DroneController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "*")
+@CrossOrigin()
 @RestController
 @RequestMapping("DroneAPI/Drone")
 public class DroneController {
